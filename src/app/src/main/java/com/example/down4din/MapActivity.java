4faab22a8d3ffa5917bd3bdbbd58be3ab733409f
Kevin Sadi <kevinsadi@live.com>
2019-10-26 15:04:36 -0400

    Many Map Updates
    It's nice now I swear
    
    Auto Lat Long
    Auto Lock In

--- src/app/src/main/java/com/example/down4din/MapActivity.java
+++ src/app/src/main/java/com/example/down4din/MapActivity.java
@@ -1,6 +1,11 @@
 package com.example.down4din;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,9 +16,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
+    private String address;
+    private String name;
+    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
+        bundle = getIntent().getExtras();
+        address = bundle.getString("address");
+        name = bundle.getString("name");
 
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
@@ -37,12 +54,58 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 //        });
     }
 
+
+
+
+
+    public LatLng getLocFromAdd(Context context, String add){
+        Geocoder gc = new Geocoder(context);
+        List<Address> address;
+        LatLng latLng = null;
+        try {
+            // May throw an IOException
+            address = gc.getFromLocationName(add, 5);
+            if (address == null) {
+                return null;
+            }
+
+            if (address.size() == 0) {
+                return null;
+            }
+
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+            Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        return latLng;
+
+    }
+
+
+
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        LatLng boltonHall = new LatLng(33.9510 ,  83.3775);
-        map.addMarker(new MarkerOptions().position(boltonHall).title("Bolton Dining Hall"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(boltonHall));
+        LatLng temp = getLocFromAdd(this, address);
+
+        LatLng location = new LatLng(temp.latitude, temp.longitude);
+        map.addMarker(new MarkerOptions().position(location).title(address));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16.0f));
+
+//        LatLng boltonHall = new LatLng(33.9510 ,  83.3775);
+//        map.addMarker(new MarkerOptions().position(boltonHall).title("Bolton Dining Hall"));
+//        map.moveCamera(CameraUpdateFactory.newLatLng(boltonHall));
     }
 }
