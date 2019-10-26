52b7a1a44f3e364cc0dea02c73c9ae2b1e260c43
mr625 <52550481+mr625@users.noreply.github.com>
2019-10-26 08:02:20 -0400

    Many things have been changed

---
+++ src/app/src/main/java/com/example/down4din/EntryAdapter.java
@@ -0,0 +1,44 @@
+package com.example.down4din;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class EntryAdapter extends ArrayAdapter {
+
+    ArrayList<Map<String, Object>> entries;
+
+    public EntryAdapter(Context context, int textViewResourceId, ArrayList<Map<String, Object>> objects) {
+        super(context, textViewResourceId, objects);
+        entries = objects;
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = convertView;
+        v = inflater.inflate(R.layout.list_item, null);
+
+        TextView main = v.findViewById(R.id.whoAndWhat);
+        TextView address = v.findViewById(R.id.address);
+        Button maps = v.findViewById(R.id.mapsBtn);
+        Button msg = v.findViewById(R.id.msgBtn);
+
+        Map<String, Object> entry = entries.get(position);
+        main.setText(String.format("%s is %s", entry.get("name"), entry.get("doing")));
+        address.setText(entry.get("address").toString());
+        return v;
+    }
+}
---
+++ src/app/src/main/java/com/example/down4din/HomeActivity.java
@@ -0,0 +1,48 @@
+package com.example.down4din;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ListView list = findViewById(R.id.list);
+        final ArrayList<Map<String, Object>> entriesList = new ArrayList<>();
+
+        CollectionReference collection = FirebaseFirestore.getInstance()
+                .collection("test_users");
+        collection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    List<DocumentSnapshot> allDocuments = task.getResult().getDocuments();
+                    for (int i = 0; i < allDocuments.size(); i++) {
+                        entriesList.add(allDocuments.get(i).getData());
+                    }
+                }
+            }
+        });
+
+        EntryAdapter adapter = new EntryAdapter(this, R.layout.list_item, entriesList);
+        list.setAdapter(adapter);
+    }
+}
